5e1ada2aa3220a70c89cb9ef05b58966381f135d
nagappan <nagappan08@gmail.com>
2020-07-27 15:08:03 +0530

    Initial commit for angular material workout

---
+++ material-app-workout/src/app/app.component.ts
@@ -0,0 +1,75 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ICoronaStats } from 'src/app/coronastats';
+import { MatSort, Sort } from '@angular/material/sort';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  
+  title = 'Country Wide Corona Status Tracker'
+
+  displayedColumns: string[] = [
+    'Country',
+    'TotalConfirmed',
+    'NewConfirmed',
+    'TotalDeaths',
+    'NewDeaths',
+    'TotalRecovered',
+    'NewRecovered',
+    'Date',
+  ]
+
+  coronastats: ICoronaStats[] = [];
+
+  @ViewChild(MatSort) matsort: MatSort;
+
+  @ViewChild(MatSort, {static: false}) 
+  set sort(sort: MatSort) {
+    sort.sortChange.subscribe((sort: Sort) => {
+      console.log('sortChange', sort.active);
+      console.log('sortChange', sort.direction);
+      console.log(this.coronastats);
+      this.coronastats = this.coronastats.sort((cs1, cs2) => {
+        if (sort.direction === 'asc') {
+          return cs1[sort.active] - cs2[sort.active];
+        } else {
+          return cs2[sort.active] - cs1[sort.active];
+        }
+      });
+      this.coronastats = [...this.coronastats];
+      console.log(this.coronastats);    
+    });
+
+    this.matsort = sort;
+    // this.dataSource.sortingDataAccessor = (data, sortheaderid) => {
+    //   console.log(sortheaderid);
+    //   return data[sortheaderid];
+    // };
+ }
+
+  ngOnInit() {
+    this.fetchCoronaStats();
+  }
+
+  constructor(private httpclient: HttpClient) {
+
+  }
+
+  fetchCoronaStats(): void {
+    this.httpclient.get("https://api.covid19api.com/summary").subscribe((response: any)=>{
+      console.log("response ===>", response.Countries)
+      this.coronastats = response.Countries
+      this.coronastats.forEach((stat: ICoronaStats, index, arr)=> {
+        let percentage: BigInteger = (stat.TotalRecovered / stat.TotalConfirmed) * 100  
+        console.log(stat.Country + "==" +  percentage +  "==" +  (percentage > 50));
+        if (percentage > 50) {
+          stat.isMedicalGood = true;
+        } 
+      })
+    });
+  }
+}
---
+++ material-app-workout/src/app/coronastats.ts
@@ -0,0 +1,10 @@
+export interface ICoronaStats {
+    Country: String;
+    NewConfirmed: Number;
+    TotalConfirmed: Number;
+    NewDeaths: Number;
+    TotalDeaths: Number;
+    NewRecovered: Number;
+    TotalRecovered: Number;
+    Date: String;
+  }
